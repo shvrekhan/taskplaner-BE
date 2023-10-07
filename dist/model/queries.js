ceec32e40658d1b56b8f280a34bb0395bd123e86
shvrekhan <shvrekhan@gmail.com>
2023-10-07 23:56:57 +0530

    query upgraded

--- dist/model/queries.js
+++ dist/model/queries.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const query = {
-    create: "CREATE TABLE login ( user_id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (100) NOT NULL, created_at TIMESTAMP DEFAULT current_timestamp, last_login TIMESTAMP)"
+    create: "SELECT *",
 };
 exports.default = query;
 //# sourceMappingURL=queries.js.map
--- src/model/queries.ts
+++ src/model/queries.ts
@@ -1,5 +1,4 @@
 const query = {
-  create: "CREATE TABLE login ( user_id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (100) NOT NULL, created_at TIMESTAMP DEFAULT current_timestamp, last_login TIMESTAMP)"
+  create: "SELECT *",
 };
 export default query;
-
