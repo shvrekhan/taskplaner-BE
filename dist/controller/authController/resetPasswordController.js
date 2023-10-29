9c49d0ac0417636a6081acb548696aece7d8f6af
shvrekhan <shvrekhan@gmail.com>
2023-10-29 17:38:49 +0530

    smtp cred removed.

--- dist/controller/authController/resetPasswordController.js
+++ dist/controller/authController/resetPasswordController.js
@@ -1,15 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: "task planer",
-        pass: "Taskplaner@987654321",
-    },
-});
 const message = {
     from: "your_name noreplaytaskplaner@gmail.com",
     to: "recipient_name shvrekhan@gmail.com",
@@ -18,15 +9,6 @@ const message = {
 };
 const resetPassword = (req, res) => {
     console.log(req, "poiu");
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.status(200).json({ msg: 'email sent' });
-            console.log("Email sent successfully!");
-        }
-    });
 };
 exports.default = resetPassword;
 //# sourceMappingURL=resetPasswordController.js.map
--- src/controller/authController/resetPasswordController.ts
+++ src/controller/authController/resetPasswordController.ts
@@ -1,15 +1,7 @@
 const nodemailer = require('nodemailer');
 import { Request, Response } from "express";
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: "task planer",
-        pass: "Taskplaner@987654321",
-    },
-});
+
 
 const message = {
     from: "your_name noreplaytaskplaner@gmail.com",
@@ -20,14 +12,7 @@ const message = {
 
 const resetPassword = (req: Request, res: Response) => {
     console.log(req, "poiu");
-    transporter.sendMail(message, (err: any, info: any) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200).json({ msg: 'email sent' });
-            console.log("Email sent successfully!");
-        }
-    });
+
 }
 
 export default resetPassword;
