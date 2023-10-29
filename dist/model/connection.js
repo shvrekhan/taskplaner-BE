a94e160aebca420a05eb25f0a20473d8e407422a
shvrekhan <shvrekhan@gmail.com>
2023-10-29 17:54:10 +0530

    connection config changed.

--- dist/model/connection.js
+++ dist/model/connection.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = __importDefault(require("pg"));
-const config_1 = require("../config/config");
 const { Pool } = pg_1.default;
+// const pool = new Pool({
+//   host: host,
+//   user: user,
+//   password: password,
+//   database: database,
+// });
 const pool = new Pool({
-    host: config_1.host,
-    user: config_1.user,
-    password: config_1.password,
-    database: config_1.database,
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 exports.default = pool;
 //# sourceMappingURL=connection.js.map
--- src/model/connection.ts
+++ src/model/connection.ts
@@ -3,11 +3,14 @@ import { password, user, database, host } from "../config/config";
 
 const { Pool } = pg;
 
+// const pool = new Pool({
+//   host: host,
+//   user: user,
+//   password: password,
+//   database: database,
+// });
 const pool = new Pool({
-  host: host,
-  user: user,
-  password: password,
-  database: database,
-});
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 export default pool;
