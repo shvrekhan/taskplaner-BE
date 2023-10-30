80af5006e481be1d6d542b885805c20b95c5b9fb
shvrekhan <shvrekhan@gmail.com>
2023-10-30 22:10:17 +0530

    origin updated in cors

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,9 @@ const authRouter_1 = __importDefault(require("./router/authRouter"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: 'https://taskplaner-fe.vercel.app/'
+}));
 app.get("/", (req, res) => {
     res.status(200).json({ SUCCESS: "Task Planer BE Module." });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://taskplaner-fe.vercel.app/'
+}));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ SUCCESS: "Task Planer BE Module." });
