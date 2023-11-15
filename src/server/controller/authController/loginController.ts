f942fd95c028253c7afb5e73746761b5d478cfb3
shvrekhan <shvrekhan@gmail.com>
2023-11-16 00:46:17 +0530

    B.E 1.2 => login api added.

--- src/server/controller/authController/loginController.ts
+++ src/server/controller/authController/loginController.ts
@@ -5,18 +5,19 @@ const bcrypt = require("bcrypt");
 
 const loginUser = async (req: Request, res: Response) => {
     try {
-        const { username, email, password } = req.body;
+        const { email, password } = req.body;
         const usersList: User[] = await checkIfUserExists(req.body);
 
         if (usersList.length === 0) {
             return res.status(404).json({ msg: "User not found" });
         }
-
         const storedPassword = usersList[0].password;
         const isPasswordValid = await bcrypt.compare(password, storedPassword);
 
         if (isPasswordValid) {
-            const token = generateAuthToken(username);
+            const token = generateAuthToken(email);
+            res.cookie("27_YYYYY_GGGGG", token, { expires: new Date(Date.now() + 1000 * 60 * 60), httpOnly: true, secure: false })
+            res.cookie("23_AAAAA_888888", usersList[0], { expires: new Date(Date.now() + 1000 * 60 * 60 * 60), httpOnly: true, secure: false })
             return res.status(200).json({ status: 'Success', token });
         } else {
             return res.status(401).json({ status: 'Fail', msg: 'Password mismatch' });
