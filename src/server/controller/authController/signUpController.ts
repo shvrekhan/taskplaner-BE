d6f9ae6a9c6cb6057d173ae1f905df32e9625022
shvrekhan <shvrekhan@gmail.com>
2023-11-16 00:45:25 +0530

    B.E 1.1 => sign up api updated cookies set in headers.

--- src/server/controller/authController/signUpController.ts
+++ src/server/controller/authController/signUpController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { checkIfUserExists, hashUserPassword, generateUUID, saveUserData, generateAuthToken, mapUserInfoInResponseObject } from "../commonRouterFunctions"
+import { checkIfUserExists, hashUserPassword, generateUUID, saveUserData, generateAuthToken, mapUserInfoInResponseObject, encryptJwt } from "../commonRouterFunctions"
 import { userSignUpSchema } from "../../schema/schema";
 
 const signUpUser = async (req: Request, res: Response) => {
@@ -9,7 +9,7 @@ const signUpUser = async (req: Request, res: Response) => {
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
     }
-    let { email, password, userName } = req.body;
+    let { email, password } = req.body;
     let isUserPresent = (await checkIfUserExists(req.body)).length;
     if (isUserPresent) {
       res.status(400).json("User already exists.")
@@ -20,10 +20,12 @@ const signUpUser = async (req: Request, res: Response) => {
       const user = await saveUserData(userId, req.body, hashPassword, created_at);
       const token = generateAuthToken(email);
       const userInfo = mapUserInfoInResponseObject(userId, req.body, created_at);
-      res.status(200).json({ accessToken: token, user: userInfo });
+      const encryptedJwtToken = encryptJwt(token);
+      res.cookie("27_YYYYY_GGGGG", token, { expires: new Date(Date.now() + 1000 * 60 * 60), httpOnly: true, secure: false })
+      res.cookie("23_AAAAA_888888", userInfo, { expires: new Date(Date.now() + 1000 * 60 * 60 * 60), httpOnly: true, secure: false })
+      res.status(200).json({ msg: "Success" });
     }
   } catch (error) {
-    console.error("Error during user registration:", error);
     res.status(500).json({ error: "An internal server error occurred." });
   }
 };
